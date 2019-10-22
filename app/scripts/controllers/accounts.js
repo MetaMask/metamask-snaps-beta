bd1922bd610d0e67050b3b9756c7a6a2b1c7b69b
Dan Finlay <dan@danfinlay.com>
2019-10-22 10:26:52 -0700

    Begin routing signing methods to plugins

--- app/scripts/controllers/accounts.js
+++ app/scripts/controllers/accounts.js
@@ -1,6 +1,8 @@
 const ObservableStore = require('obs-store')
 const EventEmitter = require('safe-event-emitter')
 const extend = require('xtend')
+const sigUtil = require('eth-sig-util')
+const normalizeAddress = sigUtil.normalize
 
 /**
  * Accounts Controller
@@ -16,19 +18,78 @@ class AccountsController extends EventEmitter {
   constructor (opts = {}) {
     super()
 
+    const { keyringController, pluginAccountsController } = opts
+    this.keyringController = keyringController
+    this.pluginAccountsController = pluginAccountsController
+
     const initState = extend({
       accountRings: [],
     }, opts.initState)
     this.store = new ObservableStore(initState)
   }
 
+  async getAccounts() {
+    const keyAccounts = await this.keyringController.getAccounts()
+    const pluginAccounts = this.pluginAccountsController.resources.map(acct => acct.address)
+    return [...keyAccounts, ...pluginAccounts]
+  }
+
+  async exportAccount (address) {
+    return this.keyringController.exportAccount(address)
+  }
+
+  async removeAccount (address) {
+    return this.keyringController.removeAccount(address)
+  }
+
+  async fullUpdate () {
+    const update = await this.keyringController.fullUpdate()
+    const pluginAccounts = this.pluginAccounts.resources
+
+    const pluginTypes = pluginAccounts.filter((account) => {
+      return account.fromDomain
+    })
+    update.keyringTypes = update.keyringTypes.concat(pluginTypes)
+
+    const pluginKeyrings = pluginTypes.map((domain) => {
+      const accounts = pluginAccounts.filter((account) => {
+        return account.fromDomain === domain
+      })
+      .map(account => account.address)
+
+      return {
+        type: domain,
+        accounts,
+      }
+    })
+
+    update.keyrings = update.keyrings.concat(pluginKeyrings)
+    return update
+  }
+
+  async signMessage (msgParams) {
+    try {
+      return this.keyringController.signMessage(msgParams)
+    } catch (err) {
+      const address = normalizeAddress(msgParams.from)
+      if (!this.pluginManagesAddress(address)) {
+        throw new Error('No keyring or plugin found for the requested account.')
+      }
+
+      throw new Error('MetaMask needs to impelment this method for plugins.')
+    }
+  }
+
+  pluginManagesAddress (address) {
+    const pluginAccounts = this.pluginAccounts.resources
+    const normalized = pluginAccounts.map(acct => normalizeAddress(acct.address))
+    return normalized.includes(address)
+  }
+
   /**
    * TO IMPLEMENT:
    */
-  async getAccounts() {}
-  exportAccount () {}
-  removeAccount (address) {}
-  async fullUpdate () {}
+
 
   async signMessage (msgParams) {}
   async signPersonalMessage (msgParams) {}
