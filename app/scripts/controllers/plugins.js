e7c9748685d9e4beae58912cf8aabd7fb0a91982
Dan Finlay <dan@danfinlay.com>
2019-12-17 08:28:51 -0800

    Add comma

--- app/scripts/controllers/plugins.js
+++ app/scripts/controllers/plugins.js
@@ -410,7 +410,7 @@ class PluginsController extends EventEmitter {
         setInterval,
 
         // Todo: Remove ethers.js, just for convenience:
-        ethers
+        ethers,
 
         // Typed Arrays:
         Int8Array,
